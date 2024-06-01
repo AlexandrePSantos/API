7882e651172571e6fb93037e3328262254320ea9
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-06-02 00:09:48 +0100

    Update user.js

--- routes/cm/user.js
+++ routes/cm/user.js
@@ -7,9 +7,9 @@ const authMiddleware = require('../../middlewares/auth');
 
 //user CRUD
 userRouter.get('/', controller.getAll); //read all
-userRouter.get('/:idToken', controller.getById); //read one by his id
+userRouter.get('/:idUser', controller.getById); //read one by his id
 userRouter.post('/create', controller.create); //create new user
 userRouter.put('/update', controller.update); //update user
-userRouter.delete('/delete/:idToken', controller.delete); //delete user
+userRouter.delete('/delete/:idUser', controller.delete); //delete user
 
 module.exports = userRouter;
