73ee210f5e7890b502250ac25abcdac93cc87288
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-06-02 00:18:30 +0100

    Update auth.js

--- controllers/cm/auth.js
+++ controllers/cm/auth.js
@@ -28,7 +28,7 @@ exports.signin = async (req, res) => {
 
             if (passwordIsValid) {
                 const accessToken = authenticateUtil.generateAccessToken({ id: user.id, name: user.name });
-                res.status(200).json({ name: user.name, token: accessToken });
+                res.status(200).json({ idUser: user.idUser, name: user.name, token: accessToken });
                 return;
             }
         } else {
